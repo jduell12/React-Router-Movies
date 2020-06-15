d371042dc4bbfe4767331d68eea4051bb9325091
Jessica <duellje@oregonstate.edu>
2020-06-15 16:11:31 -0400

    completed first stretch - making code DRY by creating movie card component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {useRouteMatch, useParams} from 'react-router-dom'
+import MovieCard from './MovieCard'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
-  const {url, path} = useRouteMatch();
   const {id} = useParams();
  
   useEffect(() => {
@@ -35,25 +35,7 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <MovieCard movie = {movie}/>
   );
 }
 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link, useRouteMatch, useParams} from 'react-router-dom'
+import MovieCard from './MovieCard'
 
 const MovieList = props => {
   return (
@@ -17,23 +18,9 @@ function MovieDetails({ movie }) {
   
   return (
     <Link to={`/movies/${movie.id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+        <MovieCard movie={movie}></MovieCard>
     </Link>
+    
   );
 }
 
